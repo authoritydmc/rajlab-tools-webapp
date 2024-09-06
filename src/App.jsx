eec05b14793ceec3adc1c3cf5829535de9dc709f
Raj Dubey <raj@rajlabs.in>
2024-09-06 23:05:13 +0530

    fix : rewrite routing to properly work

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-
-import RootLayout from './RootLayout';
-import MainLayout from './MainLayout';
-import { ThemeProvider } from './providers/ThemeContext';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainToolPage from './components/MainToolPage';
 import TextSanitize from './components/text-Sanitize/text-Sanitize';
+import MainLayout from './MainLayout';
+import { ThemeProvider } from './providers/ThemeContext';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />, 
+    children: [
+      {
+        path: "/",
+        element: <MainToolPage />,
+      },
+      {
+        path: "/tools/sanitize-text",
+        element: <TextSanitize />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  const route=createBrowserRouter([
-    {
-      path:"/",
-      element:<MainToolPage/>
-    },
-    {
-      path:"tools/sanitize-text",
-      element:<TextSanitize/>
-    }
-  ])
   return (
     <ThemeProvider>
-      <RootLayout>
-        <MainLayout>
-         <RouterProvider router={route}/>
-        </MainLayout>
-      </RootLayout>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
-
-
 
 export default App;
--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { useTheme } from "./providers/ThemeContext";
-
+import { Link } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 // Main layout component for applying theme and other styling
 function MainLayout({ children }) {
   const { isDarkMode, toggleDarkMode } = useTheme();
@@ -8,13 +9,14 @@ function MainLayout({ children }) {
     <div
       className={`flex flex-col ${
         isDarkMode ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"
-      } transition-colors duration-300`}
+      } transition-colors duration-300 min-h-screen`} // Ensure full height
     >
       <header className="flex items-center justify-center p-6 sm:p-8 bg-gradient-to-r from-purple-600 to-indigo-600 relative">
-        <h1 className="text-2xl sm:text-3xl font-bold text-center text-white">
+        <Link to="/" className="text-2xl sm:text-3xl font-bold text-center text-white">
           Rajlab Utilities
-        </h1>
+        </Link>
         <button
+          type="button"
           className={`absolute top-4 right-4 p-3 rounded-full transition-colors duration-300 ${
             isDarkMode ? "bg-gray-700" : "bg-gray-300"
           }`}
@@ -24,8 +26,8 @@ function MainLayout({ children }) {
           {isDarkMode ? "🌞" : "🌙"}
         </button>
       </header>
-      <main>
-        {children}
+      <main className="flex-1">
+      <Outlet />
       </main>
     </div>
   );
