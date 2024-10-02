33ee827fca5dc78310aa110941dedc60b2b8a7d6
Raj Dubey <raj@rajlabs.in>
2024-10-02 20:32:24 +0530

    chore:add FaKey icon

--- src/utils/iconMap.jsx
+++ src/utils/iconMap.jsx
@@ -1,11 +1,11 @@
-import { FaCog, FaImage, FaLink, FaList } from "react-icons/fa";
+import { FaCog, FaImage, FaKey, FaLink, FaList } from "react-icons/fa";
 import { BiCategoryAlt } from "react-icons/bi";
 import { MdCleaningServices } from "react-icons/md";
 import { CgFormatText } from "react-icons/cg";
 import { FaHashtag } from "react-icons/fa6";
 import { AiOutlineFieldString } from "react-icons/ai";
 const iconMap = {
-    FaList, BiCategoryAlt, FaCog, FaImage, FaLink,MdCleaningServices,CgFormatText,FaHashtag,AiOutlineFieldString
+    FaList, BiCategoryAlt, FaCog, FaImage, FaLink,MdCleaningServices,CgFormatText,FaHashtag,AiOutlineFieldString,FaKey
 }
 
 
